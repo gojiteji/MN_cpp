64eebffc5e9432912c5d6572d5dba99424883e24
gojiteji <mail@gojiteji.com>
2020-11-12 01:57:31 +0900

    6-1 done

--- 6/kadai_6_1.c
+++ 6/kadai_6_1.c
@@ -1,33 +1,14 @@
-#include<stdio.h>
-#include<stdlib.h>
-const long int a = 3399;
-const long int b = 48271;
-const long int c = 44488;
-const long int d = 2147483647;
+#include <stdio.h>
 
-long long int rand2(long int *seed){
-    long long int acc = (long long int)*seed%44488*48271-3399*((*seed)/44488);
-    *seed = acc;//create next
-    return acc;
+long long int rand2(long int *seed)
+{
+ long long int acc = (long long int)*seed*48271LL;
+ *seed = acc%2147483647;
+ return acc;
 }
 
-void rand0(long int *j)
-{
-long int k = (*j) / c;
-printf("b:%ld\n", (*j ));
-*j = b * (*j - k * c) - a * k;
-if (*j < 0)
-*j += d; }
 
-
-
-
-int main(){
-    long int ss=100;
-	long int *s=&ss;
-	
-	printf("%lld\n",rand2(s));
-
-
-	return 0;
+int main(void){
+    // Your code here!
+    
 }
