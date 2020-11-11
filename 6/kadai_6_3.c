7ce14d55f34f83eb50835c7d2690a7b3464b6e12
gojiteji <mail@gojiteji.com>
2020-11-12 04:01:09 +0900

    とりあえずdone

--- 6/kadai_6_3.c
+++ 6/kadai_6_3.c
@@ -1,120 +1,179 @@
-#include <stdio.h> #include <stdlib.h> #include <math.h>
-/* 行列の最大次数 */ #define NEQ 16
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <math.h>
+/* 行列の最大次数 */ 
+#define NEQ 16 
 typedef double ElemType;
-int Jacobi( int n,
-/* 行列の次数 */
-/* 入力値 (正方対称行列)配列 */ /* 固有ベクトル格納配列 */
-/* 繰り返し処理上限回数 */ /* 収束判定の閾値 */
-/* 回転作業用行列 */
-/* 固有ベクトル作業配列 */
-) {
-ElemType A[NEQ][NEQ], ElemType X[NEQ][NEQ]
-const int maxloop = 1000; const double eps = 1.0e-6; ElemType A2[NEQ][NEQ]; ElemType X2[NEQ][NEQ];
-int i, j, cnt = 0;
-/* カウンタ */
-/* 固有ベクトル計算用単位行列のセット */ for (i = 0; i < n; i++) {
-for (j = 0; j < n; j++) {
-  X[i][j] = (i == j) ? 1.0 : 0.0; }
-}
-/* 繰り返し処理 */
-for (cnt = 0; cnt < maxloop; cnt++) {
-/* p, q 変数 */
-int p = 0, q = 0; /* sin, cos 変数 */ ElemType sn, cs;
-/* 非対角要素の最大要素の探索 */ ElemType max = 0.0;
-for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
-            if (i != j) {
-if (fabs(A[i][j]) > max) {
-/* 最大値、位置インデックスの保持 (p < q) */ max = fabs(A[i][j]); q=; p=;
-} }
-} }
-/* 収束判定閾値より小さくなれば処理終了 */ if (max < eps)
-  
+int Jacobi(int n,
+           /* 行列の次数 */
+           /* 入力値 (正方対称行列)配列 */ /* 固有ベクトル格納配列 */
+           /* 繰り返し処理上限回数 */      /* 収束判定の閾値 */
+                                           /* 回転作業用行列 */
+                                           /* 固有ベクトル作業配列 */
+           ElemType A[NEQ][NEQ],
+           ElemType X[NEQ][NEQ])
+{
+  const int maxloop = 1000;
+  const double eps = 1.0e-6;
+  ElemType A2[NEQ][NEQ];
+  ElemType X2[NEQ][NEQ];
+  int i, j, cnt = 0;
+  /* カウンタ */
+  /* 固有ベクトル計算用単位行列のセット */ 
+  for (i = 0; i < n; i++)
   {
-ElemType alpha = 0.5 * (A[p][p] - A[q][q]); ElemType beta = -A[p][q];
-ElemType gamma = ; cs = sqrt(0.5 * (1.0 + gamma));
-sn = sqrt(0.5 * (1.0 - gamma));
-if (alpha * beta < 0)
-sn = -sn;
-}
-/* 行列に回転演算を施して、要素を 0 にする処理 */ for (i = 0; i < n; i++) {
-    for (j = 0; j < n; j++) {
-        if ((i == p || i == q) && (j == p || j == q)) {
-if (i == p && j == p)
-/* App の計算 */
-A2[i][j] = ;
-else if (i == q && j == q)
-/* Aqq の計算 */
-A2[i][j] = ;
-else
-/* Apq, Aqp の計算 */
-A2[i][j] = ;
+    for (j = 0; j < n; j++)
+    {
+      X[i][j] = (i == j) ? 1.0 : 0.0;
+    }
+  }
+  /* 繰り返し処理 */
+  for (cnt = 0; cnt < maxloop; cnt++)
+  {
+    /* p, q 変数 */
+    int p = 0, q = 0; /* sin, cos 変数 */
+    ElemType sn, cs;
+    /* 非対角要素の最大要素の探索 */ 
+    ElemType max = 0.0;
+    for (i = 0; i < n; i++)
+    {
+      for (j = 0; j < n; j++)
+      {
+        if (i != j)
+        {
+          if (fabs(A[i][j]) > max)
+          {
+            /* 最大値、位置インデックスの保持 (p < q) */ 
+            max = fabs(A[i][j]);
+            q = i>j?i:j;
+            p = i<j?i:j;
+          }
         }
-        else if (i == p || j == p) {
-;
-    
-  /* Api, Aip の要素の計算 */
-int k = (j == p) ? i : j;
-A2[i][j] = ;
-        }
-        else if (i == q || j == q) {
-/* Aqi, Aiq の要素の計算 */
-int k = (j == q) ? i : j;
-A2[i][j] = ;
-} else
-} }
-  A2[i][j] = A[i][j];
-/* 固有ベクトル値の計算 */ for (i = 0; i < n; i++) {
-    for (j = 0; j < n; j++) {
-        if (j == p)
-X2[i][p] = X[i][p] * cs - X[i][q] * sn; else if (j == q)
-            X2[i][q] = X[i][q] * cs + X[i][p] * sn;
-        else
-            X2[i][j] = X[i][j];
-} }
-/* 作業領域からの書き戻し */ for (i = 0; i < n; i++) {
+      }
+    }
+    /* 収束判定閾値より小さくなれば処理終了 */ 
+    if (max < eps)
+     return 0;
 
-  for (j = 0; j < n; j++) {
-A[i][j] = ; X[i][j] = ;
-} }
+    {
+      ElemType alpha = 0.5 * (A[p][p] - A[q][q]);
+      ElemType beta = -A[p][q];
+      ElemType gamma = fabs(alpha)/sqrt(alpha*alpha+beta*beta);
+      cs = sqrt(0.5 * (1.0 + gamma));
+      sn = sqrt(0.5 * (1.0 - gamma));
+      if (alpha * beta < 0)
+        sn = -sn;
+    }
+    /* 行列に回転演算を施して、要素を 0 にする処理 */ 
+    for (i = 0; i < n; i++)
+    {
+      for (j = 0; j < n; j++)
+      {
+        if ((i == p || i == q) && (j == p || j == q))
+        {
+          if (i == p && j == p)
+            /* App の計算 */
+            A2[i][j] = cs*(cs*A[p][p]-sn*A[p][q])-sn*(cs*A[p][q]-sn*A[q][q]);
+          else if (i == q && j == q)
+            /* Aqq の計算 */
+            A2[i][j] = sn*(sn*A[p][p]+cs*A[p][q])+cs*(sn*A[p][q]+cs*A[q][q]);
+          else
+            /* Apq, Aqp の計算 */
+            A2[i][j] = sn*(cs*A[p][p]-sn*A[p][q])+cs*(cs*A[p][q]-sn*A[q][q]);
+        }
+        else if (i == p || j == p)
+        {
+          /* Api, Aip の要素の計算 */
+          int k = (j == p) ? i : j;
+          A2[i][j] = cs*A[i][k]-sn*A[i][k];
+        }
+        else if (i == q || j == q)
+        {
+          /* Aqi, Aiq の要素の計算 */
+          int k = (j == q) ? i : j;
+          A2[i][j] = sn*A[i][k]+cs*A[i][k];
+        }
+        else
+        A2[i][j] = A[i][j];
+      }
+    }
+    
+    /* 固有ベクトル値の計算 */ for (i = 0; i < n; i++)
+    {
+      for (j = 0; j < n; j++)
+      {
+        if (j == p)
+          X2[i][p] = X[i][p] * cs - X[i][q] * sn;
+        else if (j == q)
+          X2[i][q] = X[i][q] * cs + X[i][p] * sn;
+        else
+          X2[i][j] = X[i][j];
+      }
+    }
+    /* 作業領域からの書き戻し */ for (i = 0; i < n; i++)
+    {
+
+      for (j = 0; j < n; j++)
+      {
+        A[i][j] = A2[i][j];
+        X[i][j] = X2[i][j];
+      }
+    }
+  }
+  return -1;
 }
-return -1; }
-/* 使用する行列の型宣言 */ typedef struct {
-/* 正方行列の要素数 */
-int n;
-/* 正方行列 (対称行列) */ ElemType mat[NEQ][NEQ];
-/* 固有ベクトル (列ベクトル) */ ElemType vec[NEQ][NEQ];
+/* 使用する行列の型宣言 */ typedef struct
+{
+  /* 正方行列の要素数 */
+  int n;
+  /* 正方行列 (対称行列) */ ElemType mat[NEQ][NEQ];
+  /* 固有ベクトル (列ベクトル) */ ElemType vec[NEQ][NEQ];
 } Matrix;
 /* 行列データの定義 */ Matrix testData[] = {
-{3, {{1, 3, 0}, {3, 2, 1}, {0, 1, 3}}},
-{4, {{0,2,1,2}, {2,3,0,0}, {1,0,0,1}, {2,0,1,0}}},
-  
-  {4, {{0,1,0,1}, {1,2,0,1}, {0,0,0,0}, {1,1,0,0}}} };
-/* データ数 */
-const int dataSize = sizeof (testData) / sizeof (Matrix);
-int main() {
-int idx;
-for (idx = 0; idx < dataSize; idx++) { /* カウンタ */
-int i, j;
-/* 行列データ */
-Matrix data = testData[idx];
-printf("--------------\n"); /* 入力行列の表示 */ printf("Matrix\n");
-for (i = 0; i < data.n; i++) {
-for(j = 0; j < data.n; j++) { printf("%8f ", data.mat[i][j]);
-} printf("\n");
+    {3, {{1, 3, 0}, {3, 2, 1}, {0, 1, 3}}},
+    {4, {{0, 2, 1, 2}, {2, 3, 0, 0}, {1, 0, 0, 1}, {2, 0, 1, 0}}},
 
+    {4, {{0, 1, 0, 1}, {1, 2, 0, 1}, {0, 0, 0, 0}, {1, 1, 0, 0}}}};
+/* データ数 */
+const int dataSize = sizeof(testData) / sizeof(Matrix);
+int main()
+{
+  int idx;
+  for (idx = 0; idx < dataSize; idx++)
+  { /* カウンタ */
+    int i, j;
+    /* 行列データ */
+    Matrix data = testData[idx];
+    printf("--------------\n"); /* 入力行列の表示 */
+    printf("Matrix\n");
+    for (i = 0; i < data.n; i++)
+    {
+      for (j = 0; j < data.n; j++)
+      {
+        printf("%8f ", data.mat[i][j]);
+      }
+      printf("\n");
+    }
+    /* Jacobi 法による、固有値・固有ベクトルの導出 */ 
+    int retcode = Jacobi(data.n, data.mat, data.vec);
+    printf("retcode: %d\n", retcode);
+    if (retcode < 0)
+      printf("Matrix Does Not Converge.\n");
+    else if (retcode == 0)
+    {
+      /* 対角要素の表示 */ printf("Eigenvalue\n");
+      for (i = 0; i < data.n; i++)
+        printf(" %8f ", data.mat[i][i]);
+      /* 列ベクトルの表示 */ printf("\nEigenvector\n");
+      for (i = 0; i < data.n; i++)
+      {
+        for (j = 0; j < data.n; j++)
+        {
+          printf("%8f ", data.vec[i][j]);
+        }
+        printf("\n");
+      }
+    }
+  }
+  return 0;
 }
-/* Jacobi 法による、固有値・固有ベクトルの導出 */ int retcode = Jacobi(data.n, data.mat, data.vec); printf("retcode: %d\n", retcode);
-if (retcode < 0)
-printf("Matrix Does Not Converge.\n");
-else if (retcode == 0) {
-/* 対角要素の表示 */ printf("Eigenvalue\n");
-for (i = 0; i < data.n; i++)
-printf(" %8f ", data.mat[i][i]);
-/* 列ベクトルの表示 */ printf("\nEigenvector\n");
-for (i = 0; i < data.n; i++) {
-for(j = 0; j < data.n; j++) { printf("%8f ", data.vec[i][j]);
-}
-printf("\n"); }
-} }
-return 0; }
--- 6/kadai_6_4.c
+++ 6/kadai_6_4.c
@@ -1,7 +1,10 @@
-#include <stdio.h> #include <math.h>
+#include <stdio.h> 
+#include <math.h>
+#define M_PI 3.14159265358979323846264338327
 const int N_MAX = 64;
 void fft(int n, float arr_r[], float arr_i[]) {
-float ev_r[N_MAX], ev_i[N_MAX]; /* 偶数成分 FFT 部分系列 */ float od_r[N_MAX], od_i[N_MAX]; /* 奇数成分 FFT 部分系列 */
+float ev_r[N_MAX], ev_i[N_MAX]; /* 偶数成分 FFT 部分系列 */ 
+float od_r[N_MAX], od_i[N_MAX]; /* 奇数成分 FFT 部分系列 */
 /* 回転因子の角度 */
 float theta = 2 * M_PI / n; /* カウンタ */
 int nh, j;
@@ -9,27 +12,33 @@ int nh, j;
         return;
 /* N/2 */
   nh = n / 2;
-for (   ) {
+for (j=0;j<nh-1;j++) {
 float xr, xi, wr, wi;
-/* 偶数成分を計算し、格納する */ ev_r[j] = arr_r[j] + arr_r[nh + j]; ev_i[j] = arr_i[j] + arr_i[nh + j]; /* 奇数成分を計算する */
-xr =   ;
-xi =   ;
+/* 偶数成分を計算し、格納する */ 
+ev_r[j] = arr_r[j] + arr_r[nh + j];
+ev_i[j] = arr_i[j] + arr_i[nh + j];
+ /* 奇数成分を計算する */
+xr =  arr_r[j] - arr_r[nh + j];
+xi =  arr_i[j] - arr_i[nh + j];
 /* 係数を求める */
-wr =   ;
-wi =   ;
+wr =   (ev_r[j]+xr)*cos(theta);
+wi =   (ev_i[j]+xi)*cos(theta);
 /* 奇数成分に係数を乗じて、格納する */ od_r[j] = xr * wr - xi * wi;
 od_i[j] = xi * wr + xr * wi;
 }
-/* 偶数成分の FFT を帰納的に求める */ fft(   );
-/* 奇数成分の FFT を帰納的に求める */ fft(   );
+/* 偶数成分の FFT を帰納的に求める */ fft(n, ev_r,ev_i);
+/* 奇数成分の FFT を帰納的に求める */ fft(n, od_r,od_i);
 /* 得られた FFT 部分系列を書き戻す */ for (j = 0; j < nh; j++) {
-arr_r[2 * j] = ev_r[j]; /* 偶数成分 実数部分 */ arr_i[2 * j] = ev_i[j]; /* 偶数成分 虚数部分 */
+arr_r[2 * j] = ev_r[j]; /* 偶数成分 実数部分 */ 
+arr_i[2 * j] = ev_i[j]; /* 偶数成分 虚数部分 */
 
-; /* 奇数成分 実数部分 */
-; /* 奇数成分 虚数部分 */
- } }
+arr_r[2 * j+1] = od_r[j]; /* 奇数成分 実数部分 */
+arr_i[2 * j+1] = od_i[j]; /* 奇数成分 虚数部分 */
+ } 
+}
 int main() {
-float x[N_MAX], y[N_MAX]; const int n = 64;
+float x[N_MAX], y[N_MAX]; 
+const int n = 64;
     int i;
     for (i = 0; i < n; i++) {
 x[i] = sin(4 * 2 * M_PI * i / n);
